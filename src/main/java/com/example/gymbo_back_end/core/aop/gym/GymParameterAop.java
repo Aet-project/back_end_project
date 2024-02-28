8aa16594f87dfcc13c261d3e8721ae29b527450e
leerobing <105533691+leerobing@users.noreply.github.com>
2024-02-28 23:37:43 +0900

    feat: GymParameterAop.java 구현

---
+++ src/main/java/com/example/gymbo_back_end/core/aop/gym/GymParameterAop.java
@@ -0,0 +1,12 @@
+package com.example.gymbo_back_end.core.aop.gym;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class GymParameterAop {
+
+
+}
