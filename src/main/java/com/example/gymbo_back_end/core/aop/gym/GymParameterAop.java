e87f2387ad7353615d81ec9db31403002c840e8f
leerobing <105533691+leerobing@users.noreply.github.com>
2024-03-01 18:51:43 +0900

    feat: GymParameterAop.java 구현

--- src/main/java/com/example/gymbo_back_end/core/aop/gym/GymParameterAop.java
+++ src/main/java/com/example/gymbo_back_end/core/aop/gym/GymParameterAop.java
@@ -1,6 +1,10 @@
 package com.example.gymbo_back_end.core.aop.gym;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -8,5 +12,24 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class GymParameterAop {
 
+    @Pointcut("execution(* com.example.gymbo_back_end.gym.controller..*.*(..))")
+    public void pointCut() {
 
+    }
+
+    @Before("pointCut()")
+    public void beFore(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+
+        for (Object obj : args) {
+            log.info("type : {}",obj.getClass().getSimpleName());
+            log.info("value: {}",obj);
+        }
+    }
+
+    @AfterReturning(value = "pointCut()",returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj) {
+
+        log.info("return obj : {}",returnObj);
+    }
 }
